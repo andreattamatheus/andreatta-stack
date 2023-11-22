564f64a1e90a61ca40e5b986e2e8cad16367fd9d
Matheus Andreatta <matheus.andreatta@pdmfc.com>
2023-11-22 14:56:52 -0300

    Remove files and fix html

--- resources/views/layouts/admin/dashboard.blade.php
+++ resources/views/layouts/admin/dashboard.blade.php
@@ -135,15 +135,7 @@
                                 Posts
                             </a>
                         </li>
-                        <li class="items-center">
-                            <a href="./dashboard.html"
-                                class="text-xs uppercase py-3 font-bold block text-pink-500 hover:text-pink-600">
-                                <i class="fas fa-tv mr-2 text-sm opacity-75"></i>
-                                Portfólio
-                            </a>
-                        </li>
                     </ul>
-
                 </div>
             </div>
         </nav>
@@ -152,13 +144,6 @@
                 class="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
                 <div class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
                     <form class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
-                        <div class="relative flex w-full flex-wrap items-stretch">
-                            <span
-                                class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3"><i
-                                    class="fas fa-search"></i></span>
-                            <input type="text" placeholder="Search here..."
-                                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10" />
-                        </div>
                     </form>
                     <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
                         <a class="text-blueGray-500 block" href="#pablo" onclick="openDropdown(event,'user-dropdown')">
