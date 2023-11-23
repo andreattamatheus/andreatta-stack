6a13dd8b3a85760fd0fd16f6b316d5aff16ba6e7
Matheus Andreatta <matheus.andreatta@pdmfc.com>
2023-11-22 21:53:01 -0300

    Fix css path

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
-    <link rel="shortcut icon" href="../assets/img/favicon.ico" />
+    <link rel="shortcut icon" href="{{asset('assets/img/favicon.ico')}}"/>
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
-    <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" />
-    <link rel="stylesheet" href="../assets/styles/tailwind.css" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('assets/styles/tailwind.css')}}"/>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <title>Matheus Andreatta | Blog</title>
