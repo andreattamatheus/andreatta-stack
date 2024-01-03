4a80536e2eeac3be99806fb4e66a26e551307474
Matheus Andreatta <matheus.andreatta@pdmfc.com>
2024-01-02 21:28:20 -0300

    Add telescope

--- resources/views/pages/app-ideas-collection/index.blade.php
+++ resources/views/pages/app-ideas-collection/index.blade.php
@@ -3,5 +3,12 @@
 
 @section('content')
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>App Ideas Collection</h1>
+            </div>
+        </div>
+    </div>
 
 @endsection
