312f5a52d6fba7d983be6035c0e4d46e0f313d09
Matheus Andreatta <andreattamatheus25@gmail.com>
2024-01-04 21:47:03 -0300

    Update repository card layout

--- resources/views/pages/dashboard/repository/index.blade.php
+++ resources/views/pages/dashboard/repository/index.blade.php
@@ -14,27 +14,36 @@
         <div class="mx-auto px-4 my-4">
             <div class="row">
                 @foreach ($repositories as $repository)
-                <div class="col-xl-6 col-sm-6 mb-xl-0 my-4">
+                <div class="col-xl-4 col-sm-6 mb-xl-0 my-4">
                     <div class="card">
-                    <div class="card-header p-3 pt-2">
-                        <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                            <i class="material-icons opacity-10">weekend</i>
+                        <div class="card-header p-3 pt-2">
+                            <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                <i class="material-icons opacity-10">weekend</i>
+                            </div>
+                            <div class="text-end pt-1">
+                            <p class="text-sm mb-0 text-capitalize">
+                                @foreach ($repository->topics as $topic)
+                                    {{$topic}}
+                                @endforeach
+                            </p>
+                            <h4 class="text-blueGray-500">
+                                <a href="{{$repository->html_url}}">{{$repository->name}}</a>
+                            </h4>
+                            </div>
                         </div>
-                        <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">
-                            @foreach ($repository->topics as $topic)
-                                {{$topic}}
-                            @endforeach
-                        </p>
-                        <h4 class="text-blueGray-500">
-                            <a href="{{$repository->html_url}}">{{$repository->name}}</a>
-                        </h4>
+                        <hr class="dark horizontal my-0">
+                        <div class="card-footer p-3">
+                            <p class="mb-0">{{$repository->description}}</p>
+                            <div class="d-flex align-items-center justify-content-between mt-2">
+                                <a href="{{$repository->html_url}}" class="btn btn-outline-primary btn-sm mb-0">View Project</a>
+                                <div class="avatar-group mt-2">
+                                  <a href="{{$repository->owner->html_url}}" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{$repository->owner->login}}">
+                                    <img alt="Image placeholder" src="{{$repository->owner->avatar_url}}">
+                                  </a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <hr class="dark horizontal my-0">
-                    <div class="card-footer p-3">
-                        <p class="mb-0">{{$repository->description}}</p>
-                    </div>
+
                     </div>
                 </div>
                 @endforeach
